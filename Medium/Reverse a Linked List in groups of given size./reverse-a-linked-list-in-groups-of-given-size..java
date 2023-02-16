2d8348368a18c5a1021811c210c3825464fa0205
Tanish anand <96870616+tunitx@users.noreply.github.com>
2023-02-17 00:53:37 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Reverse a Linked List in groups of given size./reverse-a-linked-list-in-groups-of-given-size..java
@@ -0,0 +1,108 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+class ReverseInSize
+{
+    static Node head;
+    
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        
+        while(t-- > 0)
+        {
+            int n = sc.nextInt();
+            int a1 = sc.nextInt();
+            Node head = new Node(a1);
+            Node tail = head;
+            for(int i = 1; i < n; i++)
+            {
+                int a = sc.nextInt();
+                // addToTheLast(new Node(a));
+                tail.next = new Node(a);
+                tail =tail.next;
+            }
+            
+            int k = sc.nextInt();
+            Solution ob = new Solution();
+            Node res = ob.reverse(head, k);
+            printList(res);
+            System.out.println();
+        }
+    }
+    
+    public static void printList(Node node)
+    {
+        while(node != null)
+        {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+    }
+    
+}
+
+
+// } Driver Code Ends
+
+
+/*node class of the linked list
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+
+*/
+
+class Solution
+{
+    public static Node reverse(Node head, int k)
+    {
+        //Your code here
+        Node curr = head;
+        Node prev_first = null;
+        boolean head_check = true;
+        while(curr!=null){
+            int count = 0;
+            Node prev = null;
+            Node first = curr;
+            while(curr!=null && count<k){
+                Node next = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = next;
+                count ++;
+            }
+            if(head_check){
+                head = prev; 
+                head_check = false;
+            }
+            else{
+                prev_first.next = prev;
+            }
+            prev_first = first;
+            
+        }
+        return head;
+    }
+}
+
