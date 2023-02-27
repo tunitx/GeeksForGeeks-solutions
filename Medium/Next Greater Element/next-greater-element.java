352ee23d30e83f4e8c6ae687bc14f1e1e229aee8
Tanish anand <96870616+tunitx@users.noreply.github.com>
2023-02-27 16:41:08 +0530

    Updated Solution - GfG to GitHub

--- Medium/Next Greater Element/next-greater-element.java
+++ Medium/Next Greater Element/next-greater-element.java
@@ -35,32 +35,54 @@ class Solution
     public static long[] nextLargerElement(long[] arr, int n)
     { 
         // Your code here
-        long ans[] = new long [arr.length];
-        long max [] = new long [arr.length];
-        long maxE =Integer.MIN_VALUE;
-       for(int i =n-1; i>=0; i--){
-        //   maxE = Math.max(max, arr[i]);
-        if(arr[i]>maxE){
-            maxE = arr[i];
+        //okay so this aint my approach but this is the gelchod tushars approach 
+        //but anyway i liked the idea he implmented so yeah cuz thats what good coders do
+        //you wont be able to understand the logic immeadiately, just give it time 
+        //and try implementing the solution by using stack next time slayyy
+    //     long ans[] = new long [arr.length];
+    //     long max [] = new long [arr.length];
+    //     long maxE =Integer.MIN_VALUE;
+    //   for(int i =n-1; i>=0; i--){
+    //     //   maxE = Math.max(max, arr[i]);
+    //     if(arr[i]>maxE){
+    //         maxE = arr[i];
+    //     }
+    //       max[i] = maxE;
+    //   }
+    //   int i=0,j =0;
+    //   while(i<arr.length && j<arr.length){
+    //       if(arr[i] ==max[i]){
+    //           ans[i] = -1;
+    //           i++;
+    //           j++;
+    //       }
+    //       else if(arr[i]<arr[j]){
+    //           ans[i] = arr[j];
+    //           i++;
+    //           j= i;
+    //       }
+    //       else{
+    //           j++;
+    //       }
+    //   }
+    
+    //   return ans;
+     Deque<Integer> stack = new ArrayDeque<>();
+        long [] ans = new long[arr.length];
+        ans[arr.length-1] = -1;
+        stack.push(arr.length-1);
+        for(int  i =arr.length-2; i>=0 ; i--){
+            while(!stack.isEmpty() && arr[stack.peek()]<=arr[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                ans[i] = -1;
+            }
+            else{
+                ans[i] = arr[stack.peek()];
+            }
+            stack.push(i);
         }
-           max[i] = maxE;
-       }
-       int i=0,j =0;
-       while(i<arr.length && j<arr.length){
-           if(arr[i] ==max[i]){
-               ans[i] = -1;
-               i++;
-               j++;
-           }
-           else if(arr[i]<arr[j]){
-               ans[i] = arr[j];
-               i++;
-               j= i;
-           }
-           else{
-               j++;
-           }
-       }
-       return ans;
+        return ans;
     } 
 }
