9d727c4326c7745e7743ed2ae9b04fecd3ced98b
Tanish anand <96870616+tunitx@users.noreply.github.com>
2023-03-01 19:05:11 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Overlapping Intervals/overlapping-intervals.java
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            String[] s = br.readLine().trim().split(" ");
+            int[][] Intervals = new int[n][2];
+            int j = 0;
+            for(int i = 0; i < n; i++){
+                Intervals[i][0] = Integer.parseInt(s[j]);
+                j++;
+                Intervals[i][1] = Integer.parseInt(s[j]);
+                j++;
+            }
+            Solution obj = new Solution();
+            int[][] ans = obj.overlappedInterval(Intervals);
+            for(int i = 0; i < ans.length; i++){
+                for(j = 0; j < ans[i].length; j++){
+                    System.out.print(ans[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public int[][] overlappedInterval(int[][] interval)
+    {
+        // Code here // Code here
+                Arrays.sort(interval, (a,b)-> a[0]-b[0]);
+        List<int[] > list = new ArrayList<>();
+        int start = interval[0][0];
+        int end = interval[0][1];
+        for(int [] i : interval){
+            if(end>=i[0]){
+                end = Math.max(end, i[1]);
+            }
+            else{
+                list.add(new int[]{start , end});
+                start = i[0];
+                end =  i[1];
+            }
+        }
+        list.add(new int[]{start, end});
+       return list.toArray(new int[0][]);
+
+    }
+}
