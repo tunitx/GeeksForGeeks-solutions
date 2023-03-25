c30478c1959b3a6a870955fdaadc38de6b8e896b
Tanish anand <96870616+tunitx@users.noreply.github.com>
2023-03-25 23:30:23 +0530

    Updated Solution - GfG to GitHub

--- Medium/Diameter of a Binary Tree/diameter-of-a-binary-tree.java
+++ Medium/Diameter of a Binary Tree/diameter-of-a-binary-tree.java
@@ -117,21 +117,20 @@ class GfG {
 
 class Solution {
     // Function to return the diameter of a Binary Tree.
+    int res = Integer.MIN_VALUE;
     int diameter(Node root) {
         // Your code here
+        //my own solution
+        height(root);
+        return res;
+    }
+    int height(Node root){
         if(root==null){
             return 0;
         }
-        int d1 = 1 + height(root.left) + height(root.right);
-        int d2 = diameter(root.left);
-        int d3 = diameter(root.right);
-        return Math.max(d1, Math.max(d2, d3));
+        int l = height(root.left);
+        int r = height(root.right);
+        res = Math.max(l+r+1, res);
+        return 1 + Math.max(l,r);
     }
-    int height(Node root){
-        if(root== null){
-            return 0;
-        }
-        return 1 + Math.max(height(root.left), height(root.right));
-    }
-    
 }
