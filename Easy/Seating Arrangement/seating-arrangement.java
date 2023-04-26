be0a63a6ae6b7244bf1aa8fc0f8d462bc571845e
Tanish anand <96870616+tunitx@users.noreply.github.com>
2023-04-26 23:36:44 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Seating Arrangement/seating-arrangement.java
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter out=new PrintWriter(System.out);
+        int test = Integer.parseInt(in.readLine().trim());
+        while(test-- > 0){
+            int n = Integer.parseInt(in.readLine().trim());
+            int m = Integer.parseInt(in.readLine().trim());
+            String s[]=in.readLine().trim().split(" ");
+            int [] seats = new int[m];
+            for(int i = 0; i < m; i++){
+                seats[i] = Integer.parseInt(s[i]);
+            }
+            Solution obj = new Solution();
+            boolean res = obj.is_possible_to_get_seats(n, m, seats);
+            
+            String _result_val = (res) ? "Yes" : "No";
+            out.println(_result_val);
+        }
+        out.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    public static boolean is_possible_to_get_seats(int n, int m, int[] seats) {
+        // code here
+        
+        if(m==1 && seats[0]==0 && n==1)
+        return true;
+          for(int i=0;i<m-1;i++)
+          {
+                 if(i==0)
+                 {
+                       if(seats[i]==0 && seats[i+1]!=1)
+                       {
+                             seats[i]=1;
+                             n--;
+                       }
+                 }
+                 else{
+                        if(i>0 && seats[i]==0 && seats[i-1]!=1 && seats[i+1]!=1)
+                        {
+                             seats[i]=1;
+                              n--;
+                        }
+                 }
+                 if(n<1)
+                 break;
+          }
+         
+         if(n>=1)  //check this condition 0 0 0 1 0 0
+
+                       //for last two element because we are running the loop till m-1
+         {
+              if(seats[m-1]== 0 && seats[m-2]==0)
+          {
+                n--;
+          }
+         }
+         
+         
+          if(n==0)
+          return true;
+          else
+          return false;
+    }
+}
