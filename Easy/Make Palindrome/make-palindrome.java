6bd9e2b7c2ccdfebe531ddabb8272db777c311e7
Tanish anand <96870616+tunitx@users.noreply.github.com>
2023-05-03 20:06:33 +0530

    Updated Solution - GfG to GitHub

--- Easy/Make Palindrome/make-palindrome.java
+++ Easy/Make Palindrome/make-palindrome.java
@@ -54,36 +54,46 @@ class GFG {
 class Solution {
     public static boolean makePalindrome(int n, String[] arr) {
         // code here
+        //date : 3rd may 2023
+        //simple hashSet implementation
+        boolean check = true;
+        boolean secCheck = true;
+        String checker = arr[0];
+        for(int i =1; i<arr.length ; i++){
+            if(!(checker.equals(arr[i]))){
+                secCheck = false;
+            }
+        }
+        if(secCheck && arr.length>1){
+            return true;
+        }
         HashSet<String> set = new HashSet<>();
-        
         for(int i =0; i<arr.length; i++){
             String og = arr[i];
-            if(set.contains(og)){
-                //  System.out.println("s");
-                continue;
-            }
+            if(set.contains(og) && og.length()!=1) continue;
             else{
                 StringBuilder rev = new StringBuilder();
-                rev.append(og);
-                rev.reverse();
-                String revStr = rev.toString();
-                if(revStr.equals(og)){
-                    // System.out.println(revStr);
+                String revStr =  rev.append(og).reverse().toString();
+                if (revStr.equals(og) && check ){
+                    check = false;
                     continue;
                 }
-               else if(set.contains(revStr)){
-                //   System.out.println("t");
-                    set.remove(revStr);
-                }else{
-                    // System.out.println(og);
-                    set.add(og);
-                }
+                else if (set.contains(revStr)) {
+                    
+                    set.remove(revStr);     } 
+                else{
+                    set.add(og);   
+                } 
             }
         }
-        if(!set.isEmpty()){
-            return false;
+    //   if(set.size()==1){
+    //         StringBuilder rev = new StringBuilder();
+    //         String revStr =  rev.append(og).reverse().toString();
+    //         if(rev)
+    //   }
+      return set.isEmpty();
         }
-        return true;
+    // return false;
     }
-}
+
         
