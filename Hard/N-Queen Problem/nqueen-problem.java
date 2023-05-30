ed3e64588cf3c3a7152e48cad353b36fbffa53f7
Tanish anand <96870616+tunitx@users.noreply.github.com>
2023-05-30 22:43:47 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/N-Queen Problem/nqueen-problem.java
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+class GFG{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(in.readLine());
+        while(t-- > 0) {
+            int n = Integer.parseInt(in.readLine());
+            
+            Solution ob = new Solution();
+            ArrayList<ArrayList<Integer>> ans = ob.nQueen(n);
+            if(ans.size() == 0)
+                System.out.println("-1");
+            else {
+                for(int i = 0;i < ans.size();i++){
+                    System.out.print("[");
+                    for(int j = 0;j < ans.get(i).size();j++)
+                        System.out.print(ans.get(i).get(j) + " ");
+                    System.out.print("] ");
+                }
+                System.out.println();
+            }
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution{
+    static int [][] mat;
+    static ArrayList<ArrayList<Integer>> nQueen(int n) {
+        // code here
+    ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+    mat = new int[n][n];
+    solveRec(0, n, ans);
+    // Collections.sort(ans, (a,b)-> a.get(0)-b.get(0));
+    return ans;
+    }
+    static void  solveRec(int col, int n,  ArrayList<ArrayList<Integer>> ans){
+        if(col == n){
+            ArrayList< Integer> temp = new ArrayList<>();
+            for(int i =0; i<n; i++){
+                for(int j =0; j<n; j++){
+                    if(mat[j][i] ==1){
+                        temp.add(j+1);
+                    }
+                }
+            }
+            ans.add(temp);
+            return ;
+        }
+        for(int i =0; i<n; i++){
+              
+            if(isSafe(i, col, mat,n)){
+                mat[i][col] = 1;
+                solveRec(col+1, n, ans);
+                mat[i][col] = 0;
+                  
+                }
+               
+            }
+             return;
+        }
+       
+
+    
+    static boolean isSafe(int row, int col, int [][] board, int n){
+
+              int x =row;
+        int y=col;
+     //row
+      while(y>=0){
+          if(board[x][y]==1)
+          return false;
+           y--;
+      }
+     //upper left diagonal
+     x=row;
+     y=col;
+     while(y>=0 && x>=0){
+          if(board[x][y]==1)
+          return false;
+           y--;
+           x--;
+      }
+     x=row;
+     y=col;
+     //lower left diagonal
+     while(y>=0 && x<n){
+          if(board[x][y]==1)
+          return false;
+           y--;
+           x++;
+      }
+      return true;
+
+    }
+}
