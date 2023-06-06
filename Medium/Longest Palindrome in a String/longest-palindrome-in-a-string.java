5ecfd73c7fd8cb9fedc23f0481147396108f0acd
Tanish anand <96870616+tunitx@users.noreply.github.com>
2023-06-06 21:04:57 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Longest Palindrome in a String/longest-palindrome-in-a-string.java
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+class GFG
+{
+    public static void main(String args[])throws IOException
+    {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while(t-- > 0)
+        {
+            String S = read.readLine();
+            
+            Solution ob = new Solution();
+            System.out.println(ob.longestPalin(S));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    static String longestPalin(String S){
+        // code here
+          int n = S.length();
+        boolean dp [][] = new boolean[n][n];
+        
+        for(int i =0; i<n; i++){
+            dp[i][i] = true;
+        }
+         int start =0;
+        int end =0;
+boolean firstCheck = true;
+        for(int i =0; i<n-1; i++){
+            if(S.charAt(i)== S.charAt(i+1)){
+                dp[i][i+1] = true;
+                if(firstCheck){
+                start = i;
+                end = i+1;
+                firstCheck = false;
+            }
+            }
+            
+        }
+        // System.out.println(S.substring(start, end+1));
+       
+        
+        for(int k =3; k<=n; k++){
+            
+            for(int i =0; i<n-k+1; i++){
+                int j = i+k-1;
+                
+                if(dp[i+1][j-1] && S.charAt(i)== S.charAt(j)){
+                    dp[i][j] = true;
+                    
+                    if(k>(end-start+1)){
+                        start = i;
+                        end = j;
+                    }
+                }
+            }
+        }
+        return S.substring(start, end+1);
+        
+    }
+}
