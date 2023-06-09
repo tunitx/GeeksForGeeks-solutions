b52eda616e1f561500f7703cd62a1b522a878d38
Tanish anand <96870616+tunitx@users.noreply.github.com>
2023-06-09 17:27:38 +0530

    Updated Solution - GfG to GitHub

--- Medium/Coin Change/coin-change.java
+++ Medium/Coin Change/coin-change.java
@@ -25,31 +25,22 @@ class GfG {
 // User function Template for Java
 
 class Solution {
+    long res =0;
     public long count(int coins[], int N, int sum) {
-        // code here.
-        //2nd submission 
-        //date: 1 march 2023
-        long [][] mat = new long [coins.length][sum+1];
-        for(int i =0; i<=sum; i++){
-            if(i%coins[0]==0){
-                mat[0][i] =1;
-            }
-            else{
-                mat[0][i] = 0;
-            }
-        }
-        for(int i =1; i<coins.length; i++){
-            mat[i][0] =1;
-            for(int j =0; j<=sum; j++){
-                if(coins[i]>j){
-                    mat[i][j] = mat[i-1][j];
-                }
-                else{
-                    mat[i][j] = mat[i][j-coins[i]] +mat[i-1][j];
-                }
-            }
-        }
-        return mat[coins.length-1][sum];
-        
+     var dp = new long [coins.length][sum+1];
+     for(int i =0; i<sum+1; i++){
+         if(i%coins[0]==0){
+             dp[0][i] = 1;
+         }
+     }
+     for(int i =1; i<dp.length; i++){
+         for(int j =0; j<dp[0].length; j++){
+             dp[i][j] = dp[i-1][j];
+             if(coins[i]<=j){
+                 dp[i][j]+=dp[i][j-coins[i]];
+             }
+         }
+     }
+     return dp[N-1][sum];
     }
 }
