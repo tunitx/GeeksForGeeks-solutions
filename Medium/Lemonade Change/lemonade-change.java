fa78f897c2de465d05caa7b2806ac044f072ecf4
Tanish anand <96870616+tunitx@users.noreply.github.com>
2023-06-22 23:32:51 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Lemonade Change/lemonade-change.java
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.math.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int a[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            Solution obj = new Solution();
+            boolean ans = obj.lemonadeChange(n, a);
+            System.out.println(ans ? "True" : "False");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static boolean lemonadeChange(int N, int bills[]) {
+        // code here
+       int five =0, ten =0;
+       for(int i : bills){
+           if(i==5){
+               five++;
+           }
+           if(i==10){
+               if(five==0) return false;
+               five --;
+               ten++;
+           }
+           if(i==20){
+               if(ten>0){
+                   ten--;
+                   five--;
+               }
+               else{
+                   five = five-3;
+               }
+               if(five<0 || ten<0) return false;
+           }
+       }
+       if(five>=0 && ten>=0) return true;
+       return false;
+    }
+}
