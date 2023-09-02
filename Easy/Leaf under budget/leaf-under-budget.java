6a1b0f9642b381cbcb15491bbf564be66ccd3a1e
Tanish anand <96870616+tunitx@users.noreply.github.com>
2023-09-02 23:46:42 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Leaf under budget/leaf-under-budget.java
@@ -0,0 +1,144 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+import java.lang.*; 
+
+
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+
+class GFG {
+    
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    public static void inorder(Node root){
+        if(root == null)
+            return;
+        
+        inorder(root.left);
+        System.out.print(root.data+" ");
+        inorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+		while(t-- > 0){
+		    String s = br.readLine();
+	    	Node root = buildTree(s);
+	    	int k = Integer.parseInt(br.readLine());
+		    Solution obj = new Solution();
+		    int res = obj.getCount(root,k);
+		    System.out.println(res);
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node {
+    int data;
+    Node left, right;
+    
+    public Node(int data){
+        this.data = data;
+    }
+}
+*/
+class Pair{
+    Node node;
+    int count;
+    Pair(Node node,int count){
+        this.node=node;
+        this.count=count;
+    }
+} 
+class Solution{
+    public int getCount(Node node, int bud)
+    {
+        int count=0,sum=0;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(node,1));
+        while(!q.isEmpty()){
+             Node currNode=q.peek().node;
+             int lev=q.peek().count;
+             q.remove();
+             if(currNode.left==null & currNode.right==null){
+                   count++;
+                   sum+=lev;
+                   if(sum>bud)return count-1;
+                   if(sum==bud)return count;
+               }
+             if(currNode.left!=null )q.add(new Pair(currNode.left,lev+1));
+             if(currNode.right!=null)q.add(new Pair(currNode.right,lev+1));
+        }
+       
+       return count;
+    }
+}
