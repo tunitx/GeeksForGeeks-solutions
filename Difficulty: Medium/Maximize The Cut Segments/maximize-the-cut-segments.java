ce534cb526d1b97c64db7da433c28ce75f94c9ab
Tanish <96870616+tunitx@users.noreply.github.com>
2024-08-11 00:47:41 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximize The Cut Segments/maximize-the-cut-segments.java
+++ Difficulty: Medium/Maximize The Cut Segments/maximize-the-cut-segments.java
@@ -52,12 +52,27 @@ class Solution
        //date : 10 aug 2024
        
        //Memoisation based solution
-       dp = new int[n+1];
-      Arrays.fill(dp,-1);
-       int res = helper(n,x,y,z);
-       if(res<0) return 0;
-       return res;
-    //   return res ?res!=Integer.MIN_VALUE:-1;
+    //   dp = new int[n+1];
+    //   Arrays.fill(dp,-1);
+    //   int res = helper(n,x,y,z);
+    //   if(res<0) return 0;
+    //   return res;
+    
+    dp = new int[n+1];
+ 
+    // Arrays.fill(dp,-1);
+       dp[0] = 0;
+    for(int i =1; i<=n; i++){
+        dp[i] = -1;
+        if(i-x>=0) dp[i] = Math.max(dp[i], dp[i-x]);
+        if(i-y>=0) dp[i] = Math.max(dp[i], dp[i-y]);
+        if(i-z>=0) dp[i] = Math.max(dp[i], dp[i-z]);
+        
+        if(dp[i]>=0) dp[i]++;
+        
+    }
+    return dp[n] < 0 ? 0 :dp[n];
+    // return dp[n];
     }
     public int helper(int sum, int x, int y, int z){
         if(sum==0) return dp[sum]=0;
