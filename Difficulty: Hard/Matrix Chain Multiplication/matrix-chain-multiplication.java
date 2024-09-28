c800d794515e42a3f7d3447faf6dea051e8bfc98
Tanish <96870616+tunitx@users.noreply.github.com>
2024-09-28 12:45:35 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
+++ Difficulty: Hard/Matrix Chain Multiplication/matrix-chain-multiplication.java
@@ -33,23 +33,25 @@ class Solution{
     static int matrixMultiplication(int N, int arr[])
     {
         // code here
-        //date : 15th augsut 2024
+        dp = new int[N][N];
+        for(int row [] : dp) Arrays.fill(row, -1);
         
-        //MCM dp
-        dp = new int[arr.length][arr.length];
-        for(int i =0; i<dp.length; i++) Arrays.fill(dp[i],-1);
-        return helper(arr, 1, N-1);
+        return helper(1,N-1, arr);
     }
-        
-
-    static int helper(int [] nums, int i, int j){
+    
+    public static int helper(int i, int j, int arr[]){
         if(i==j) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-         int res = Integer.MAX_VALUE;
-        for(int k = i; k<j; k++){
-            int temp = Math.min(res, helper(nums, i,k) + helper(nums, k+1, j) + nums[i-1]*nums[k]*nums[j]);
-            res = Math.min(res, temp);
+        
+        int min_steps = Integer.MAX_VALUE;
+        for(int k =i; k<j; k++){
+            int steps = arr[i-1] * arr[k] * arr[j]
+            + helper(i, k, arr)
+            + helper(k+1, j, arr);
+            
+            min_steps = Math.min(min_steps, steps);
         }
-        return dp[i][j] =  res;
+        
+        return dp[i][j] = min_steps;
     }
 }
